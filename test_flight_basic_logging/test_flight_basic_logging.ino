f308e19e263f20709ecca6e82e03a8732ce71d5f
Jamie Sanson <jamie.sanson@outlook.com>
2016-05-13 11:55:34 +1200

    Add basic microSD logging capability

--- test_flight_basic_logging/test_flight_basic_logging.ino
+++ test_flight_basic_logging/test_flight_basic_logging.ino
@@ -9,6 +9,11 @@
     - 9-Axis motion sensor MPU9250 Shield
   * Components Outputted to:
     - BOB-00544 microSD card SPI breakout
+      * SD card attached to SPI bus as follows:
+      ** MOSI - pin TBD
+      ** MISO - pin TBD
+      ** CLK - pin TBD
+      ** CS - pin TBD
     
 
   Created 13 May 2016
@@ -19,10 +24,31 @@
 
 */
 
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 0; // Update this with chip select
+const String fileName = "flight_log.txt";
+
 void setup() {
-  
+  // see if the card is present and can be initialized
+  if (!SD.begin(chipSelect)) {
+    // don't do anything more
+    return;
+  }
 }
 
 void loop() {
+
+  // Read sensors and build log string here
+
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  if (dataFile) { // Check to see if file is available
+    // Add formatted data here
+    dataFile.print(""); 
+    // Finalise file write
+    dataFile.close(); 
+  }
  
 }
