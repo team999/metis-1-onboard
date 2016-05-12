3c745692637ac054e41f2bdaa11097711eced417
Jamie Sanson <jamie.sanson@outlook.com>
2016-05-13 11:36:35 +1200

    Initial code commit

---
+++ test_flight_basic_logging/test_flight_basic_logging.ino
@@ -0,0 +1,28 @@
+/*
+  Test flight basic logging
+
+  This sketch contains logic to read sensor values via I2C and dump data to an SD card 
+  for further analysis on landing. It also communicates basic data over the 433MHz radio band
+
+  The circuit:
+  * Components supplying input to device:
+    - 9-Axis motion sensor MPU9250 Shield
+  * Components Outputted to:
+    - BOB-00544 microSD card SPI breakout
+    
+
+  Created 13 May 2016
+  By Jamie Sanson
+  
+  Modified 13 May 2016
+  By Jamie Sanson
+
+*/
+
+void setup() {
+  
+}
+
+void loop() {
+ 
+}
