b40587580b3e85ddbe1f7fffeb58695f43b78afb
Jamie Sanson <jamie.sanson@outlook.com>
2016-06-13 15:00:08 +1200

    Keep track of init state

--- test_flight_basic_logging/test_flight_basic_logging.ino
+++ test_flight_basic_logging/test_flight_basic_logging.ino
@@ -23,32 +23,232 @@
   By Jamie Sanson
 
 */
+<<<<<<< Updated upstream
 
 #include <SPI.h>
 #include <SD.h>
+=======
+#include <i2c_t3.h>
+#include <SD.h>
+#include <SD_t3.h>
+#include <SPI.h>
+#include <MPU9250_helper.h>
+
+// REGION CONFIGURATION
+uint8_t OSR = ADC_8192;
+int8_t Ascale = AFS_16G;
+int8_t Gscale = GFS_250DPS;
+int8_t Mscale = MFS_16BITS;  // Choose either 14-bit or 16-bit magnetometer resolution
+int8_t Mmode = 0x02; // 2 for 8Hz, 6 for 100Hz continuous
+
+boolean serialDebug = false;
+const int sdChipSelect = 10;
+// END REGION
+
+// REGION SENSOR FIELDS
+uint16_t Pcal[8];         // calibration constants from MS5637 PROM registers
+unsigned char nCRC;       // calculated check sum to ensure PROM integrity
+uint32_t D1 = 0, D2 = 0;  // raw MS5637 pressure and temperature data
+double dT, OFFSET, SENS, T2, OFFSET2, SENS2;  // First order and second order corrections for raw S5637 temperature and pressure data
+
+int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
+int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
+int16_t magCount[3];    // Stores the 16-bit signed magnetometer sensor output
+
+double Temperature, Pressure; // stores MS5637 pressures sensor pressure and temperature
+float altitudeOffset;
+
+float aRes, gRes, mRes;
+float ax,ay,az, gx, gy, gz, mx, my, mz;
+
+// Biases resulting from calibration
+float gyroBias[3] = {0, 0, 0}, accelBias[3] = {0, 0, 0};
+float magCalibration[3] = {0, 0, 0}, magbias[3] = {0, 0, 0};
+// END REGION
+
+// REGION DATA LOGGING
+File datafile;
+String initFileName = "init/init";
+String dataFileName = "data/data";
+char fileNameBuffer[20];
+boolean isInitState = true;
+boolean initialiseOK = true;
+// END REGION
+>>>>>>> Stashed changes
 
 const int chipSelect = 0; // Update this with chip select
 const String fileName = "flight_log.txt";
 
 void setup() {
+<<<<<<< Updated upstream
   // see if the card is present and can be initialized
   if (!SD.begin(chipSelect)) {
     // don't do anything more
     return;
+=======
+  Wire1.begin(I2C_MASTER, 0x00, I2C_PINS_29_30, I2C_PULLUP_EXT, I2C_RATE_400);
+  if (serialDebug) {
+    // block until serial sent to micro
+    Serial.begin(115200);
+    while(!Serial.available()){}
+  }
+
+  setupSD();
+  setupMPU9250();
+  setupAK8963();  
+  setupMS5637();
+
+  // Block if not initialised properly (Also set LED state)
+  while (!initialiseOK);
+  
+  // Initialise CSV columns
+  isInitState = false;
+  printData("ACCEL_X,ACCEL_Y,ACCEL_Z,GYRO_X,GYRO_Y,GYRO_Z,MAG_X,MAG_Y,MAG_Z,ALT,TIME,\n");
+}
+
+void loop() {  
+  helper.readAccelData(accelCount);
+  helper.readGyroData(gyroCount);
+  helper.readMagData(magCount);
+  
+  aRes = helper.getAccelRes();
+  gRes = helper.getGyroRes();
+  mRes = helper.getMagRes();
+  
+  ax = (float)accelCount[0]*aRes;
+  ay = (float)accelCount[1]*aRes;
+  az = (float)accelCount[2]*aRes; 
+
+  gx = (float)gyroCount[0]*gRes;
+  gy = (float)gyroCount[1]*gRes;
+  gz = (float)gyroCount[2]*gRes; 
+  
+  // Calculate the magnetometer values in milliGauss
+  // Include factory calibration per data sheet and user environmental corrections
+  mx = (float)magCount[0]*mRes*magCalibration[0] - magbias[0];  // get actual magnetometer value, this depends on scale being set
+  my = (float)magCount[1]*mRes*magCalibration[1] - magbias[1];  
+  mz = (float)magCount[2]*mRes*magCalibration[2] - magbias[2];   
+
+  printData(getLogString(ax, ay, az)+getLogString(gx, gy, gz)+getLogString(mx, my, mz));
+  printData(String(getAltitude()) + "," + String(micros()) + ",\n");
+
+}
+
+void setupMPU9250() {
+  printData("Reading who-am-i byte of MPU9250\n");
+  byte c = helper.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);  // Read WHO_AM_I register for MPU-9250
+  
+  printData("MPU9250 I AM "); printData(String(c, HEX)); printData(", I should be "); printData(String(0x71, HEX) + "\n");
+
+  if (c == 0x71) {
+    printData("MPU9250 online\n");
+    printData("Calibrating...\n\n");
+
+    helper.calibrateMPU9250(gyroBias, accelBias);
+
+    printData("Accelerometer bias: (mg)\n"); 
+    printData("X: " + (String)(1000*accelBias[0]) + " Y: " + (String)(1000*accelBias[1]) + " Z: " + (String)(1000*accelBias[2]) + "\n");
+
+    printData("Gyro bias: (o/s)\n"); 
+    printData("X: " + (String)gyroBias[0] + " Y: " + (String)gyroBias[1] + " Z: " + (String)gyroBias[2] + "\n");
+
+    helper.initMPU9250(); 
+    printData("\nMPU9250 initialized for active data mode....\n\n");
+  } else {
+    initialiseOK = false;
+    printData("MPU9250 failed to initialise\n");
+  }
+}
+
+void setupAK8963() {
+  printData("Reading who-am-i byte of magnetometer\n");
+  byte d = helper.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);  // Read WHO_AM_I register for AK8963
+  printData("AK8963 I AM "); printData(String(d, HEX)); printData(", I should be "); printData(String(0x48, HEX) + "\n");
+
+  if (!d == 0x48) {
+    initialiseOK = false;
+    printData("AK8963 failed to initialise\n");
+  }
+
+  helper.initAK8963(magCalibration); 
+
+  printData("Calibrating...\n");
+  printData("X-Axis sensitivity adjustment value "); printData(String(magCalibration[0], 2) + "\n");
+  printData("Y-Axis sensitivity adjustment value "); printData(String(magCalibration[1], 2) + "\n");
+  printData("Z-Axis sensitivity adjustment value "); printData(String(magCalibration[2], 2) + "\n");
+  printData("\nAK8963 initialized for active data mode....\n");
+}
+
+void setupSD() {
+  if (!SD.begin(sdChipSelect)) {
+    printData("Card failed to initialise\n");
+    initialiseOK = false;
+    return;
+  } else {
+    // Detect init file on card
+    // Same number of init files as there are data files
+    Serial.println("Initialising card");
+    int fileCount = 1;
+    (initFileName + String(fileCount) + ".txt").toCharArray(fileNameBuffer, 20);
+    while(SD.exists(fileNameBuffer)) {
+      fileCount += 1;
+      (initFileName + String(fileCount) + ".txt").toCharArray(fileNameBuffer, 20);
+    }
+    initFileName = initFileName + String(fileCount) + ".txt";
+    dataFileName = dataFileName + String(fileCount) + ".csv";
+>>>>>>> Stashed changes
   }
 }
 
 void loop() {
 
+<<<<<<< Updated upstream
   // Read sensors and build log string here
+=======
+  if (nCRC != refCRC) {
+    initialiseOK = false;
+    printData("MS5637 checksum integrity failed\n");
+  }
+
+  // Calculate offset for relative altitude
+  float altitudeTemp = 0;
+  for(int i = 0; i < 16; i++) {
+    altitudeTemp += getAltitude();
+  }
+>>>>>>> Stashed changes
 
   File dataFile = SD.open(fileName, FILE_WRITE);
 
+<<<<<<< Updated upstream
   if (dataFile) { // Check to see if file is available
     // Add formatted data here
     dataFile.print(""); 
     // Finalise file write
     dataFile.close(); 
+=======
+  Pressure = (((D1*SENS)/pow(2, 21) - OFFSET)/pow(2, 15))/100;  // Pressure in mbar or kPa
+
+  return ((145366.45*(1.0 - pow((Pressure/1013.25), 0.190284)))/3.2808) - altitudeOffset; // Altitude calculation  
+}
+
+
+// -------------------------------------------------
+//                Utility functions
+// -------------------------------------------------
+void printData(String data) {
+  if (serialDebug) {
+    Serial.print(data);
+  } else {
+    if (isInitState) {
+      initFileName.toCharArray(fileNameBuffer, 20);
+    } else {
+      dataFileName.toCharArray(fileNameBuffer, 20);
+    }
+    
+    datafile = SD.open(fileNameBuffer, FILE_WRITE); 
+    datafile.print(data);
+    datafile.close();
+>>>>>>> Stashed changes
   }
  
 }
